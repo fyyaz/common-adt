a8569a9563b9844fcd7e6f8d36803dfe8d163f4e
fyyaz <fyyazk@gmail.com>
2016-03-08 22:20:05 -0500

    Fyyaz is the best

--- c++/big_integer/inc/big_integer.h
+++ c++/big_integer/inc/big_integer.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2016 William Granados
+// Special thanks to Fyyaz "The Dank Master" Khan
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
